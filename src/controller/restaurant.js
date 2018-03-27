e93f9b0a048f5e8a61e2a73118a93bb0982c6b49
EsTrevino <sttrevino2@gmail.com>
2018-03-27 15:22:39 -0700

    added update and delete operations

--- src/controller/restaurant.js
+++ src/controller/restaurant.js
@@ -46,5 +46,33 @@ export default({config, db}) => {
    });
  });
 
+ //UPDATE
+ //   /v1/restaurant/:id
+ api.put('/:id', (req,res)=>{
+   Restaurant.findById(req.params.id, (err, restaurant) =>{
+     if(err){
+       res.send(err);
+     }
+       restaurant.name = req.body.name;
+
+       restaurant.save(err =>{
+         if(err){
+           res.send(err);
+         }
+           res.json({message: "Restaurant info updated"})
+       });
+   });
+ });
+
+ //Delete - /v1/restaurant/:id
+ api.delete('/:id', (req, res)=>{
+   Restaurant.remove({_id: req.params.id}, (err, restaurant) =>{
+     if(err){
+       res.send(err);
+     }
+     res.json({message: "Restaurant successfull removed"});
+   });
+ });
+
   return api;
 }
